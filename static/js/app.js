6070ebbbe157c1c394d939fbf857cc98ef9e413f
waleedxahmed <contact.waleedahmed@gmail.com>
2020-08-12 13:09:51 -0400

    Final Commit

---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+
+// YOUR CODE HERE!
+tableData.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  //Filter your data based on the date
+  // Select the button
+var button = d3.select("#filter-btn");
+
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+
+  //Filter the data being entered
+  var filteredData = tableData.filter(x => x.datetime === inputValue);
+  tbody.html("")
+  filteredData.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
